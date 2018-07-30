4112b6e9d9e8e39093470172a43b6cf9bc3ccbce
miki1989 <mikolajheller@gmail.com>
2018-07-30 13:53:22 +0200

    Task 12.1 Random 50 numbers from 0-1000. Printing min, max values. Printing average and all numbers bigger than average.

---
+++ src/Main.java
@@ -0,0 +1,51 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Random random = new Random();
+        int x = 50;
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < x; i++) {
+            set.add(random.nextInt(1000));
+        }
+
+        /*Iterator<Integer> iterator = set.iterator();
+        while (iterator.hasNext()){
+            System.out.println(iterator.next());
+        }
+        */
+        // wyswietlanie max i min
+        Integer max = Collections.max(set);
+        System.out.println("The highest number is: " + max);
+        Integer min = Collections.min(set);
+        System.out.println("The smallest number is: "+ min);
+
+        //wyswietlanie sredniej
+
+        double sum = 0;
+        Iterator<Integer> iterator = set.iterator();
+        while (iterator.hasNext()){
+            sum += iterator.next();
+        }
+        double average = sum/set.size();
+        System.out.println("Average " + average);
+
+        //drukowanie wartości większych od średniej
+        System.out.println();
+        System.out.println("Values bigger than average: ");
+        double sum1 = 0;
+        Iterator<Integer> iterator1 = set.iterator();
+        while (iterator1.hasNext()){
+            sum1 += iterator1.next();
+        }
+        double average1 = sum1/set.size();
+        for (int y :set){
+            if(y > average1){
+                System.out.print(y + " ");
+            }
+        }
+
+
+    }
+}
